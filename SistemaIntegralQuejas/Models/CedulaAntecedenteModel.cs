cbed8f53fae229c41f1bb56b632ac4f31be0ad1c
Adair0103 <adidir7u7@gmail.com>
2026-07-13 15:40:23 -0600

    cambios para los pdf de las cedulas

--- SistemaIntegralQuejas/Models/CedulaAntecedenteModel.cs
+++ SistemaIntegralQuejas/Models/CedulaAntecedenteModel.cs
@@ -7,11 +7,27 @@ namespace SistemaIntegralQuejas.Models
         public string LugarRecepcion { get; set; }
         public DateTime FechaRecepcion { get; set; }
         public TimeSpan HoraRecepcion { get; set; }
-        public string Peticionario { get; set; }
+
+        public List<PeticionarioModel> Peticionarios { get; set; } = new List<PeticionarioModel>();
+
         public string Autoridad { get; set; }
         public string Explicacion { get; set; }
         public string Abogado { get; set; }
+
         public DateTime FechaActual => DateTime.Now;
-        public string MesActual => FechaActual.ToString("MMMM", new System.Globalization.CultureInfo("es-ES"));
+        public string MesActual => FechaActual.ToString(
+            "MMMM",
+            new System.Globalization.CultureInfo("es-ES")
+        );
+    }
+
+    public class PeticionarioModel
+    {
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+
+        public string NombreCompleto =>
+            $"{Nombre} {ApellidoPaterno} {ApellidoMaterno}".Trim();
     }
 }
